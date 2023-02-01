84b0975c0f9e66ee2cabac13d71ba0e09c4af733
Пользователь <gaparovnurba444@gmail.com>
2023-02-01 17:35:48 +0300

    good

---
+++ dom.js
@@ -0,0 +1,103 @@
+// const btn = document.querySelector('.btn')
+// const div = document.querySelector('.test')
+// const input = document.querySelector('input')
+// input.addEventListener('keydown', (e) =>{
+//     switch (e.key){
+//         case "Enter" : alert("enter басылды" )
+//             break;
+//         case "Shift" : alert( "error..")
+//     }
+// })
+
+const box1 = document.querySelector('.box1')
+const box2 = document.querySelector('.box2')
+const box3 = document.querySelector('.box3')
+const box4 = document.querySelector('.box4')
+const box5 = document.querySelector('.box5')
+const box6 = document.querySelector('.box6')
+const box7 = document.querySelector('.box7')
+const box8 = document.querySelector('.box8')
+const box9 = document.querySelector('.box9')
+
+box1.addEventListener('mousedown',  () => {
+    box1.style.background = "red"
+})
+box1.addEventListener('mouseover', () =>{
+    box1.style.background = ""
+})
+box2.addEventListener('mousedown',  () => {
+    box2.style.background = "blue"
+})
+box2.addEventListener('mouseover',  () => {
+    box2.style.background = ""
+})
+box3.addEventListener('mousedown',  () => {
+    box3.style.background = "red"
+})
+box3.addEventListener('mouseover',  () => {
+    box3.style.background = ""
+})
+box4.addEventListener('mousedown',  () => {
+    box4.style.background = "blue"
+})
+box4.addEventListener('mouseover',  () => {
+    box4.style.background = ""
+})
+box5.addEventListener('mousedown',  () => {
+    box5.style.background = "red"
+})
+box5.addEventListener('mouseover',  () => {
+    box5.style.background = ""
+})
+box6.addEventListener('mousedown',  () => {
+    box6.style.background = "blue"
+})
+box6.addEventListener('mouseover',  () => {
+    box6.style.background = ""
+})
+box7.addEventListener('mousedown',  () => {
+    box7.style.background = "red"
+})
+box7.addEventListener('mouseover',  () => {
+    box7.style.background = ""
+})
+box8.addEventListener('mousedown',  () => {
+    box8.style.background = "blue"
+})
+box8.addEventListener('mouseover',  () => {
+    box8.style.background = ""
+})
+box9.addEventListener('mousedown',  () => {
+    box9.style.background = "red"
+})
+box9.addEventListener('mouseover',  () => {
+    box9.style.background = ""
+})
+//
+// btn.addEventListener('click', () =>{
+//     div.style.background = "red"
+//     div.style.animation = "anim 600s"
+//     div.style.border = "none"
+//     btn.style.background = "red"
+// })
+//
+// div.addEventListener('mousedown', () =>{
+//     div.style.borderRadius = "10px"
+// })
+// div.addEventListener('mouseup', () =>{
+//     setTimeout(() =>{
+//         div.style. borderRadius = ""
+//     },300)
+// })
+//
+// div.addEventListener('mouseover', () =>{
+//     div.style.background = "green"
+//     div.style.background = "blue"
+//     div.style.border = "none"
+//
+// })
+// div.addEventListener('mouseleave', () =>{
+//     setTimeout(() =>{
+//         div.style.background = ''
+//     },200)
+// })
